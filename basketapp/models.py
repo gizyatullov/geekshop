12a51c8ac79cb893f795f4b52aee3c1f8eb42021
eric <_>
2022-02-04 13:33:56 +0400

    lesson_12. Product's count as signal.

--- basketapp/models.py
+++ basketapp/models.py
@@ -8,14 +8,6 @@ from mainapp.models import Product
 # Create your models here.
 
 
-class BasketQuerySet(models.QuerySet):
-    def delete(self, *args, **kwargs):
-        for object in self:
-            object.product.quantity += object.quantity
-            object.product.save()
-        super(BasketQuerySet, self).delete(*args, **kwargs)
-
-
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -61,18 +53,3 @@ class Basket(models.Model):
     @staticmethod
     def get_item(pk):
         return get_object_or_404(Basket, pk=pk)
-
-    # Object's saving method
-    def save(self, *args, **kwargs):
-        if self.pk:
-            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
-        else:
-            self.product.quantity -= self.quantity
-        self.product.save()
-        super(self.__class__, self).save(*args, **kwargs)
-
-    # Object's deleting method
-    def delete(self):
-        self.product.quantity += self.quantity
-        self.product.save()
-        super(self.__class__, self).delete()
